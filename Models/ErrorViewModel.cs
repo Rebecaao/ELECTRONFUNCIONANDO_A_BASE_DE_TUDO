21889681def7f579508343b4a54fe22e5ae520be
rebeca <rebeca@uptechnology.local>
2023-07-13 12:01:11 -0300

    VENCEMOOOOOOO

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace voltandoClasseStartup.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
